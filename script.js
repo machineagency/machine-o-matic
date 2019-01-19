f9dce5ce2aa942a6fb258a3dab243417657de362
Jasper O'Leary <jhaazpr@gmail.com>
2019-01-18 22:01:46 -0800

    reduce geometry choice

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ let mesh, geometry;
 
 let geometries = [
     new THREE.BoxBufferGeometry( 1000, 100, 200, 2, 2, 2 ),
-    new THREE.CircleBufferGeometry( 200, 32 ),
-    new THREE.CylinderBufferGeometry( 75, 75, 200, 8, 8 ),
-    new THREE.IcosahedronBufferGeometry( 100, 1 ),
-    new THREE.OctahedronBufferGeometry( 200, 0 ),
-    new THREE.PlaneBufferGeometry( 200, 200, 4, 4 ),
-    new THREE.RingBufferGeometry( 32, 64, 16 ),
-    new THREE.SphereBufferGeometry( 100, 12, 12 ),
-    new THREE.TorusBufferGeometry( 64, 16, 12, 12 ),
-    new THREE.TorusKnotBufferGeometry( 64, 16 )
 ];
 
 let options = {
@@ -25,10 +16,10 @@ let options = {
 let material = new THREE.MeshLambertMaterial({ color: 0xbed346 });
 
 let addMesh = () => {
-    if ( mesh !== undefined ) {
-        scene.remove( mesh );
-        geometry.dispose();
-    }
+    // if ( mesh !== undefined ) {
+    //     scene.remove( mesh );
+    //     geometry.dispose();
+    // }
     geometry = geometries[ options.Geometry ];
     // scale geometry to a uniform size
 
@@ -36,12 +27,16 @@ let addMesh = () => {
     let scaleFactor = 160 / geometry.boundingSphere.radius;
     geometry.scale( scaleFactor, scaleFactor, scaleFactor );
     let edges = new THREE.EdgesGeometry(geometry);
-    let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x000000 } ));
+    let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 5 } ));
     scene.add(line);
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
-}
+};
+
+let initGui = () => {
+
+};
 
 let init = () => {
     container = document.getElementById( 'container' );
@@ -74,15 +69,6 @@ let init = () => {
     container.appendChild( stats.dom );
     let geometries = {
         BoxBufferGeometry: 0,
-        CircleBufferGeometry: 1,
-        CylinderBufferGeometry: 2,
-        IcosahedronBufferGeometry: 3,
-        OctahedronBufferGeometry: 4,
-        PlaneBufferGeometry: 5,
-        RingBufferGeometry: 6,
-        SphereBufferGeometry: 7,
-        TorusBufferGeometry: 8,
-        TorusKnotBufferGeometry: 9
     };
 
     gui = new dat.GUI( { width: 350 } );
