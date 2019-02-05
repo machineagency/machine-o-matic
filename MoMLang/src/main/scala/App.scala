627eb84428ff541628259e8e901a05c2e6e5d38e
Jasper O'Leary <jhaazpr@gmail.com>
2019-02-04 18:57:10 -0800

    outfit test programs within test file

--- MoMLang/src/main/scala/App.scala
+++ MoMLang/src/main/scala/App.scala
@@ -1,8 +1,13 @@
 import MoMLang._
 
 object App {
+    val testProgram: String =
+    """
+    Machine myMachine:
+        pass
+    """
     def main(args: Array[String]): Unit = {
-        println(s"Input: ${args(0)}")
-        println(MoMParser.parseAll(MoMParser.mblock, args(0)))
+        println(s"Input Program: ${testProgram}")
+        println(MoMParser.parseAll(MoMParser.mblock, testProgram))
     }
 }
