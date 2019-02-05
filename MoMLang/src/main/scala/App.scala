86b0178d3a5d1ffa2d5358feeeebab63489cf57c
Jasper O'Leary <jhaazpr@gmail.com>
2019-02-05 11:53:25 -0800

    add real example program

--- MoMLang/src/main/scala/App.scala
+++ MoMLang/src/main/scala/App.scala
@@ -3,8 +3,24 @@ import MoMLang._
 object App {
     val testProgram: String =
     """
-    Machine myMachine:
-        pass
+    Machine xyPlottter:
+    	tool pen:
+    		accepts (x, y)
+    		position ABOVE
+
+    	stages:
+    		linear stage x1
+    		linear stage x2
+    		linear stage y
+
+    	connections:
+    		x1.platform connectsto y.left
+    		x2.platform connectsto y.right
+    		pen connectsto y.platform
+
+    Program DrawSquare:
+    	points Square source '~/points/square.csv'
+        draw Square
     """
     def main(args: Array[String]): Unit = {
         println(s"Input Program: ${testProgram}")
