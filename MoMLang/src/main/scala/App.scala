5fe9a60f9ca63b7b1f8edc6051f92cba59be9a5d
Jasper O'Leary <jhaazpr@gmail.com>
2019-02-08 16:12:19 -0800

    add a medium program to test

--- MoMLang/src/main/scala/App.scala
+++ MoMLang/src/main/scala/App.scala
@@ -1,7 +1,7 @@
 import MoMLang._
 
 object App {
-    val testProgram: String =
+    val easyProgram: String =
         """
         Machine xyPlottter {
             tool pen {
@@ -26,11 +26,53 @@ object App {
             points Square source "~/points/square.csv"
             draw Square
         }
-    """
+        """
+
+    val mediumProgram: String =
+        """
+        Machine yPlatformPrinter {
+            tool PrintHead {
+                accepts (x, y, z)
+                position ABOVE
+                motor E
+                action BeginExtrude {
+                    E.forward()
+                }
+                action BeginRetract {
+                    E.reverse()
+                }
+                action StopExtrudeOrRetract {
+                    E.stop()
+                }
+            }
+
+            stages {
+                linear stage z1
+                linear stage z2
+                linear stage x
+                linear stage y
+            }
+
+            connections {
+                z1 connectsto x.leftEdge
+                z2 connectsto x.rightEdge
+                PrintHead connectsto x.platform
+                y connectsto WORK_SURFACE BELOW
+            }
+        }
+
+        Program PrintFile {
+            points onePart source "~/points/part_sliced.csv"
+            PrintHead.BeginExtrude()
+            draw onePart
+            PrintHead.StopExtrudeOrRetract()
+        }
+        """
+
     def main(args: Array[String]): Unit = {
         println(s"Input Program...")
-        println(s"${testProgram}")
+        println(s"${mediumProgram}")
         println("Running parser...")
-        println(MoMParser.parseAll(MoMParser.program, testProgram))
+        println(MoMParser.parseAll(MoMParser.program, mediumProgram))
     }
 }
