a1a2d8b3a171c2ebd9c6dcbf16065af518552d9b
Jasper O'Leary <jhaazpr@gmail.com>
2019-02-13 21:36:55 -0800

    add configReasoner which is main, may remove though

---
+++ MoMLang/src/main/scala/ConfigReasoner.scala
@@ -0,0 +1,48 @@
+import MoMLang._
+
+object ConfigReasoner {
+
+    val easyProgram: String =
+        """
+Machine xyPlottter {
+    tool pen {
+        accepts (x, y)
+        position ABOVE
+    }
+
+    stages {
+        linear stage x1
+        linear stage x2
+        linear stage y
+    }
+
+    connections {
+        x1.platform connectsto y.left
+        x2.platform connectsto y.right
+        pen connectsto y.platform
+    }
+}
+
+Program DrawSquare {
+    points Square source "~/points/square.csv"
+    draw Square
+}
+        """
+
+    // TODO: leadscrew constants
+
+    // TODO: mapping function construction
+    // e.g. f: D(3, 3, 4) -> R(20, 40)
+
+
+    def main(args: Array[String]): Unit = {
+        println("This is config reasoner")
+        val parse = MoMParser.parseAll(MoMParser.program, easyProgram)
+        println(parse)
+        // println(MoMParser.stageStatements)
+        // println(MoMParser.connectstoStatements)
+        // MoMParser.constructComponentTree()
+        // println(MoMParser.stageNameToNode)
+        println("Done")
+    }
+}
