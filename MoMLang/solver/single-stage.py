d6c4fedcc916ded39edf7dedea35259ae52ff123
Jasper O'Leary <jhaazpr@gmail.com>
2019-02-20 22:00:10 -0800

    solver working in basic case, seems weird though

--- MoMLang/solver/single-stage.py
+++ MoMLang/solver/single-stage.py
@@ -6,27 +6,35 @@ s = Solver()
 
 # Leadscrew: PITCH rev -> 1 mm
 
-steps = Int('steps')
-revolutions = Int('revolutions')
+steps = Real('steps')
+revolutions = Real('revolutions')
 move_mm = Real('move_mm')
 stage_length_mm = Real('stage_length_mm')
 stepper_angle = Real('stepper_angle')
 leadscrew_pitch = Real('leadscrew_pitch')
+tool_pos_x = Int('tool_pos_x')
 
-# Transforms
-stepper_steps_to_rev = (revolutions == steps * (stepper_angle / 360))
+# Transforms: steps -> rev -> move_mm
+stepper_steps_to_rev = (revolutions ==  steps * (stepper_angle / 360))
 leadscrew_rev_to_mm = (move_mm == leadscrew_pitch * revolutions)
-s.add(stepper_steps_to_rev)
-s.add(leadscrew_rev_to_mm)
+s.add(And(stepper_steps_to_rev, leadscrew_rev_to_mm))
 
 # Physical parameters
 s.add(stepper_angle == 1.8)
 s.add(leadscrew_pitch == 6.096)
 s.add(stage_length_mm == 609.6)
 
-# Tool to motor constraints
+# Tool to motor constraints, very basic in this case
+# move_mm -> tool_pos_x -> GOAL
+x_axis_to_tool_pos_x = (tool_pos_x == move_mm)
+s.add(x_axis_to_tool_pos_x)
 
+# Solve inverse kinematics
+goal = (tool_pos_x == 50)
+s.add(goal)
 
-# Solve for position
-for c in s.assertions():
-    print c
+s.check()
+model = s.model()
+print model[steps]
+exit()
+
