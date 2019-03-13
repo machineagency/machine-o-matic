d30874baf3dccfac0fbca01e28bdd712aa586b95
Jasper O'Leary <jhaazpr@gmail.com>
2019-03-12 19:15:33 -0700

    [WIP] shield control from scratch bc C char parsing was too error prone

--- hardware/src/ShieldControl/shield_control.ino
+++ hardware/src/ShieldControl/shield_control.ino
@@ -1,6 +1,6 @@
 #include "AccelStepper.h"
 #include "MultiStepper.h"
-#include "jsmn.h"
+#include "ArduinoJson.h"
 #include <ctype.h>
 
 // Assumes all steppers are configured identically with the following settings:
@@ -20,45 +20,10 @@ AccelStepper phys_x_motor(AccelStepper::DRIVER, XSTEP, XDIR);
 AccelStepper phys_y_motor(AccelStepper::DRIVER, YSTEP, YDIR);
 AccelStepper phys_z_motor(AccelStepper::DRIVER, ZSTEP, ZDIR);
 
-char receive_buffer[512];
-int receive_buffer_idx = 0;
-size_t bytes_to_read = 0;
-size_t num_bytes_read = 0;
-
-jsmn_parser parser;
-int num_tok = 32;
-int num_tok_used = 0;
-jsmntok_t tokens[32];
-
-char token_string[32];
-
-char pgm_read_byte = 0;
-int num_open_braces = 0;
-
-void test_motors(void);
-
-void handle_move_inst(void);
-void handle_map_inst(void);
-
-const int NUM_STAGES = 3;
-const char* stage_names[3] = { "x1", "x2", "y" };
-AccelStepper *phys_motors[3] = { &phys_x_motor, &phys_y_motor, &phys_z_motor };
-// TODO: un-hardcode mapping
-// index is stage index, value at index is physical index
-int stage_phys_map[3] = { 0, 1, 2 };
-
-int INST_FIELD_IDX = 2;
-int STEPS_FIRST_TOK_IDX = 5;
-
-void set_substring(char *, char *, int, int);
-
-void setup()
-{
+void setup() {
     Serial.begin(9600);
     pinMode(8, OUTPUT); // Disable pin.
     digitalWrite(8, LOW);
-
-    jsmn_init(&parser);
 
     phys_x_motor.setMaxSpeed(400.0);
     phys_x_motor.setAcceleration(200.0);
@@ -68,126 +33,35 @@ void setup()
     phys_z_motor.setAcceleration(200.0);
 
     // Uncomment to test motors
-    test_motors();
+    // test_motors();
 
     Serial.println("Hello! I am the motor hub.");
 
 }
 
-void loop()
-{
-    while (Serial.available()) {
-        pgm_read_byte = Serial.read();
-        receive_buffer[receive_buffer_idx++] = pgm_read_byte;
-        Serial.println(receive_buffer);
+void loop() {
+    if (Serial.available()) {
+        String s = Serial.readString();
+        Serial.print("Got: ");
+        Serial.println(s);
 
-        if (pgm_read_byte == '{') {
-            num_open_braces += 1;
+        char *json_str = (char *) malloc((s.length() + 1) * sizeof(char));
+        s.toCharArray(json_str, s.length() + 1);
+
+        StaticJsonBuffer<256> jsonBuffer;
+        JsonObject& root = jsonBuffer.parseObject(json_str);
+
+        // Test if parsing succeeds.
+        if (!root.success()) {
+          Serial.println("parseObject() failed");
+          free(json_str);
+          return;
         }
-        if (pgm_read_byte == '}') {
-            num_open_braces -= 1;
-        }
 
-        if (num_open_braces == 0 && !isspace((int) pgm_read_byte)) {
-            num_tok_used = jsmn_parse(&parser, receive_buffer, receive_buffer_idx + 1,
-                        tokens, num_tok);
-            Serial.print("Parsed # of tokens: ");
-            Serial.println(num_tok_used);
-            Serial.println(num_tok_used);
-            for (int i = 0; i < num_tok_used - 1; i++) {
-                jsmntok_t curr_tok = tokens[i];
-                // Serial.println(curr_tok.type);
-                for (int j = curr_tok.start; j < curr_tok.end; j++) {
-                    Serial.print(receive_buffer[j]);
-                }
-                Serial.println();
-            }
-            Serial.print("INSTRUCTION: ");
-            set_substring(token_string, receive_buffer,
-                            tokens[INST_FIELD_IDX].start, tokens[INST_FIELD_IDX].end);
-            Serial.println(token_string);
+        const char *inst = root["inst"];
+        Serial.println(inst);
 
-            if(!strcmp(token_string, "move")) {
-                handle_move_inst();
-            }
+        free(json_str);
 
-            memset(receive_buffer, 0, sizeof receive_buffer);
-            receive_buffer_idx = 0;
-        }
     }
 }
-
-int str_index_of(char *cand_str, char **str_arr, int arr_len) {
-    for (int i = 0; i < arr_len; i += 1) {
-        if (!strcmp(str_arr[i], cand_str)) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void set_substring(char *substr, char *str, int begins, int ends) {
-    int sub_idx = 0;
-    for (int s_idx = begins; s_idx < ends; s_idx++) {
-        substr[sub_idx++] = str[s_idx];
-    }
-    substr[sub_idx] = '\0';
-}
-
-void handle_move_inst(void) {
-    Serial.println("Handling move command.");
-    MultiStepper steppers;
-    long steps_per_motor[NUM_STAGES];
-    int spm_idx = 0;
-
-    for (int i = STEPS_FIRST_TOK_IDX; i < num_tok_used - 1; i += 2) {
-        set_substring(token_string, receive_buffer,
-                        tokens[i].start, tokens[i].end);
-        Serial.print(token_string);
-        Serial.print("->");
-
-        int stage_idx = str_index_of(token_string, (char **) stage_names, NUM_STAGES);
-        if (stage_idx == -1) {
-            Serial.println("Error: could not find stage.");
-        }
-        int phys_motor_idx = stage_phys_map[stage_idx];
-        steppers.addStepper(*phys_motors[phys_motor_idx]);
-
-        set_substring(token_string, receive_buffer,
-                        tokens[i + 1].start, tokens[i + 1].end);
-
-        steps_per_motor[spm_idx++] = strtol(token_string, NULL, 0);
-        Serial.print(token_string);
-        Serial.print(": ");
-        Serial.println(phys_motor_idx);
-    }
-
-    Serial.println(steps_per_motor[0]);
-
-
-    // steppers.addStepper(phys_x_motor);
-    // steppers.addStepper(phys_y_motor);
-    // steppers.addStepper(phys_z_motor);
-
-    steppers.moveTo(steps_per_motor);
-    steppers.runSpeedToPosition(); // Blocks until all are in position
-    delay(1000);
-}
-
-void test_motors(void) {
-    MultiStepper steppers;
-    steppers.addStepper(*(phys_motors[0]));
-    steppers.addStepper(*(phys_motors[1]));
-
-    // yMotor.moveTo(1200);
-    // yMotor.run();
-
-    long coords[2];
-
-    coords[0] = 400;
-    coords[1] = 400;
-    steppers.moveTo(coords);
-    steppers.runSpeedToPosition(); // Blocks until all are in position
-    delay(1000);
-}
-
