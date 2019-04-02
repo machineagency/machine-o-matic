4495c254da16752ba7080a2605ce61e4e2fd238a
Jasper O'Leary <jhaazpr@gmail.com>
2019-04-01 17:28:01 -0700

    server works

--- frontend/server.js
+++ frontend/server.js
@@ -13,14 +13,14 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(express.static(__dirname + '/client')); // set the static files location /public/img will be /img for users
 
 //Store all JS and CSS in Scripts folder.
-app.use(express.static(__dirname + '/css'));
+// app.use(express.static(__dirname + '/css'));
 
 // routes ==================================================
-// require('./routes.js')(app, runningInCloud); // pass our application into our routes
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
-});
+// Example route
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/index.html'));
+// });
 
 
 // start app ===============================================
