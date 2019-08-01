0b4151d8d5795466df455e8044a1a79eba1fff02
Jasper O'Leary <jhaazpr@gmail.com>
2019-08-01 14:23:49 -0700

    rendering boilerplate

---
+++ momscript/control_panel/client/script.js
@@ -0,0 +1,98 @@
+'use strict';
+
+let container, stats;
+let stageGui, connectionGui;
+let camera, scene, renderer;
+let topDirectionalLight, leftDirectionalLight, rightDirectionalLight;
+let mesh, lines, geometry;
+let tool;
+let programText;
+let clock;
+let mixers = [];
+
+/* SCENE INITIALIZATION */
+
+let initCamera = () => {
+    let aspect = window.innerWidth / window.innerHeight;
+    let viewSize = 150;
+    camera = new THREE.OrthographicCamera(-viewSize * aspect, viewSize * aspect,
+        viewSize, -viewSize, -1000, 10000);
+    camera.zoom = 0.35;
+    camera.updateProjectionMatrix();
+    camera.frustumCulled = false;
+    camera.position.set(-500, 500, 500); // I don't know why this works
+    camera.lookAt(scene.position);
+    camera.position.set(-400, 500, 800); // Pan away to move machine to left
+};
+
+let initScene = () => {
+    scene = new THREE.Scene();
+    clock = new THREE.Clock();
+    scene.background = new THREE.Color(0x000000);
+    topDirectionalLight = new THREE.DirectionalLight( 0xffffff, 1.00 );
+    leftDirectionalLight = new THREE.DirectionalLight( 0xffffff, 0.75 );
+    rightDirectionalLight = new THREE.DirectionalLight( 0xffffff, 0.50 );
+    leftDirectionalLight.position.set(-1.0, 0.0, 0.0);
+    rightDirectionalLight.position.set(0.0, 0.0, 1.0);
+    scene.add(topDirectionalLight);
+    scene.add(leftDirectionalLight);
+    scene.add(rightDirectionalLight);
+    scene.add(new THREE.GridHelper(2000, 50, 0x444444, 0xe5e6e8));
+};
+
+let initRenderer = () => {
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+};
+
+let initStats = () => {
+    stats = new Stats();
+    container.appendChild( stats.dom );
+};
+
+let init = () => {
+    container = document.getElementById( 'container' );
+
+    initScene();
+    initCamera();
+    initRenderer();
+    //initStats();
+
+    window.addEventListener( 'resize', onWindowResize, false );
+};
+
+let onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+};
+
+let cappedFramerateRequestAnimationFrame = (framerate) => {
+    if (framerate === undefined) {
+        requestAnimationFrame(animate);
+    } else {
+        setTimeout(() => {
+            requestAnimationFrame(animate);
+        }, 1000 / framerate);
+    }
+};
+
+let animate = () => {
+    cappedFramerateRequestAnimationFrame(30);
+    render();
+    // stats.update();
+};
+
+let render = () => {
+    let deltaSeconds = clock.getDelta();
+    mixers.forEach((mixer) => {
+        mixer.update(deltaSeconds);
+    });
+    renderer.render( scene, camera );
+};
+
+init();
+animate();
