243a4df11c52620b8f8a897aaed19b988a2cb089
Jasper O'Leary <jhaazpr@gmail.com>
2019-09-08 17:23:46 -0700

    reserved word array for future use

--- momscript/control_panel/client/script.js
+++ momscript/control_panel/client/script.js
@@ -74,6 +74,16 @@ const defaultStageNames = [
     "kingman", "euclid", "mechano", "rumbler", "descartes"
 ];
 
+const reservedWords = [
+    "break", "case", "catch", "continue", "debugger", "default", "delete",
+    "do", "else", "finally", "for", "function", "if", "in", "instanceof",
+    "new", "return", "switch", "this", "throw", "try", "typeof", "var", "void",
+    "while", "with", "class", "const", "enum", "export", "extends", "import",
+    "super", "implements", "interface", "let", "package", "private",
+    "protected", "public", "static", "yield", "async", "await", "null",
+    "undefined", "true", "false", "NaN", "Infinity"
+];
+
 const DriveQualEnum = {
     LINEAR: 0,
     ROTARY: 1,
