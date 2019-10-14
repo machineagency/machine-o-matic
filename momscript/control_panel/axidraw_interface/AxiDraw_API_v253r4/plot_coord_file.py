538e409e27ae919dd296c2ed8e6c1b01e3f6b476
Jasper O'Leary <jhaazpr@gmail.com>
2019-10-14 10:16:48 -0700

    python side: coords -> new svg on the fly

--- momscript/control_panel/axidraw_interface/AxiDraw_API_v253r4/plot_coord_file.py
+++ momscript/control_panel/axidraw_interface/AxiDraw_API_v253r4/plot_coord_file.py
@@ -2,11 +2,26 @@ import sys, cmd, os
 import serial, json
 from pyaxidraw import axidraw   # import module
 
+def path_coords_to_svg(path_coords) -> str:
+    svg = '<svg width=“500” height=“500" xmlns=“http://www.w3.org/2000/svg“>'
+    # NOTE: this is an SVG with only one path, if you need more than
+    # one path, use another outer loop and update accordingly
+    svg += '\n<polygon points='
+    for coord in path_coords:
+        svg += '{:.5f},{:.5f} '.format(coord[0], coord[1])
+    svg += ' style="fill=none; stroke:black; stroke-width:1;"/>'
+    svg += '\n</svg>'
+    return svg
+
+
 with open('axidraw_interface/AxiDraw_API_v253r4/coords.txt', 'r+') as file:
     coord_text = file.read();
     coord_dict = json.loads(coord_text);
     coords = coord_dict['coords']
     file.close();
+
+print(path_coords_to_svg(coords))
+exit()
 
 ad = axidraw.AxiDraw()          # Initialize class
 ad.interactive()                # Enter interactive context
@@ -19,5 +34,5 @@ for coord in coords:
 
 ad.moveto(0, 0)
 
-ad.disconnect()  
+ad.disconnect()
 
