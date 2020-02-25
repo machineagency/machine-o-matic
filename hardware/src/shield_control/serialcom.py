00b729ef9e16980ff77869adbb49bb1af164e3e7
Khang Lee <leekg97@gmail.com>
2020-02-24 18:32:04 -0800

    change pyscript to main serial comm

--- hardware/src/shield_control/serialcom.py
+++ hardware/src/shield_control/serialcom.py
@@ -1,17 +1,108 @@
-import serial, json
+import serial, json, sys, re
+import serial.tools.list_ports
 
-port = serial.Serial('/dev/tty.usbmodem142301', 115200) # open serial port
-print("Opened " + port.name) # check which port was really used
+BAUD = 115200
 
-file_name = input("Which JSON file would you like to send? ")
-file_name += ".json"
-with open(file_name) as json_file:
-	data = json.load(json_file)
-	data_bytes = bytes(json.dumps(data), "utf-8")
-	# print(data_bytes)
-	port.write(data_bytes) # write a string
-	port.flush()
+# Main Function -----------------------------------
+def main():
+	ser = open_port() # initiate serial port connection
 
-port.close() # close port
+	while True:
+		data = read_port(ser)
+		# print(data)
+		status = 4
+		if (len(data) > 0 and re.match('^[0-9]*$', data[0])):
+			status = int(data[0])
+		else:
+			status = 4
+			
+		# print(data) # print incoming data
+		if (status == 1): # status = idle
+			msg_print(data[2:])
+			instr = input("Instruction:  ")
+			if instr[0:2] == "w ":
+				# print("reading .json file")
+				send_data(ser, instr[2:] + ".json")
+
+			elif instr == "c" or instr == "C":
+				ser.close() # close serial port
+				sys.exit() # exit for loop
+
+			else:
+				print("unable to interpret instr") 
+
+		elif (status == 2): # status = busy
+			msg_print(data[2:])
+
+		elif (status == 3): # status = error
+			print(data[2:])
+
+		else: # no data / invalid packet
+			print(data)
+
+def msg_print(msg):
+	print("------------------------------")
+	print(msg)
+
+# Function: Open Serial Port -----------------------------------
+def open_port():
+	# List out all com ports
+	ports = serial.tools.list_ports.comports()
+	print("Ports available:")
+	port = 'none'
+
+	for obj in ports: # Find the right com port
+		print(" -> " + obj.device.replace("cu", "tty"))
+		# print(obj)
+		if ("/dev/cu.usbmodem" in obj.device):
+			# port = obj.device.replace("cu", "tty")
+			port = obj.device
+	print("------------------------------------")
+	try: # Initiate serial port
+		ser = serial.Serial(port, BAUD)
+		print("Opened: " + ser.name)
+		print("------------------------------------")
+		return ser
+	except serial.serialutil.SerialException:
+		print("Unable to open port")
+		return None
+	# port = serial.Serial('/dev/tty.usbmodem142301', 115200) # open serial port
+	# print("Opened " + port.name) # check which port was really used
 
 
+# Function: Read Serial Input ----------------------------------
+def read_port(ser):
+	data = ""
+	# if (ser.inWaiting()):
+	temp = ser.readline()
+	if temp != 10:
+		data += temp.decode('utf-8')
+	
+	if len(data) != 0 and "{" in data:
+		start_i = data.find("{")+1
+		end_i = data.find("}")
+		if start_i != -1 and end_i != -1:
+			data = data[start_i:end_i] # retrieve data from packet
+		# print(data)
+		return data
+	return ""
+
+	
+
+
+# Parse JSON & Serial Write --------------------------
+def send_data(ser, file_name):
+	with open(file_name) as json_file:
+		print("------------------------------")
+		print("Loading JSON file:")
+		data = json.load(json_file)
+		print(json.dumps(data, indent=1))
+		data_bytes = bytes(json.dumps(data), "utf-8")
+		print("------------------------------")
+		print("Sending JSON file...")
+		ser.write(data_bytes) # write a string
+		ser.flush()
+		
+
+if __name__ == "__main__":
+	main()
--- hardware/src/shield_control/shield_control.ino
+++ hardware/src/shield_control/shield_control.ino
@@ -1,75 +1,29 @@
 #include "AccelStepper.h"
 #include "MultiStepper.h"
 #include "ArduinoJson.h"
+#include "shield_control.h"
 #include <ctype.h>
 
-// Assumes all steppers are configured identically with the following settings:
-// 0.9 Degree Steppers
-//   8x Microstepping Factor
-//   Max travel of +/- 90 degrees
-// Total steps count is +/- 800 steps with the above settings.
-
-// Pin Mapping
-// #define XSTEP 2
-// #define YSTEP 3
-// #define ZSTEP 4
-// #define XDIR 5
-// #define YDIR 6
-// #define ZDIR 7
-#define MAX_STAGES 5
-
-// Forward Declarations
-int string_index_of(String);
-AccelStepper& get_phys_motor(String);
-void test_motors(void);
-void zero_motors(void);
-void configure_motors(JsonObject&);
-void set_motor_kinematics(int);
-void handle_move(JsonObject&);
-void handle_multiple_moves(JsonArray&);
-
-/* Cartesian example */
-// This section declares the constants for the stepper motors.
-int NUM_STAGES = 0;
-String stage_names[3]; // array of names of each of the motors
-AccelStepper phys_motors[3]; // array of AccelStepper classes for each motor
-int stage_to_phys[3] = { 0, 1, 2 }; // array of name to AccelStepper mapping
-
-/* Polar example */
-// const int NUM_STAGES = 2;
-// String stage_names[2] = { "l", "c"};
-// AccelStepper phys_motors[3];// = { &phys_x_motor, &phys_y_motor, &phys_z_motor };
-// // TODO: un-hardcode mapping
-// // index is stage index, value at index is physical index
-// int stage_to_phys[3] = { 0, 1, 2 };
-
-// Constants
-int MS_FACTOR = 4;
-float SPEED = 200.0;
-
-
 void setup() {
-	Serial.println("Initiating Setup");
+	Serial.println("{2:Initiating Setup}");
 	Serial.begin(115200); // set serial com baud rate
 	pinMode(8, OUTPUT);   // set pin 8 as output
 	digitalWrite(8, LOW); // set pin 8 to low
-	
-    // test_motors();
-	//  if (&phys_motors[0]) Serial.println(&phys_motors[0]);
-	
-   Serial.println("Hello! I am the motor hub.");
+   // test_motors();
+   Serial.println("{1:Hello! I am the motor hub.}");
 }
-
 
 void loop() {
 	if (Serial.available()) {
-		String s = Serial.readString();
-		// s = s.substring(4); // there's an error here... 
-		Serial.print("Received: ");
-		Serial.println(s);
+		String str = Serial.readString();
+		while (Serial.available()) {
+			str = str + Serial.readString();
+		}
+		String print_s = *(new String("{2:Received: ")) + str + *(new String("}"));
+		Serial.println(print_s);
 
-		char *json_str = (char *) malloc((s.length() + 1) * sizeof(char));
-		s.toCharArray(json_str, s.length() + 1);
+		char *json_str = (char *) malloc((str.length() + 1) * sizeof(char));
+		str.toCharArray(json_str, str.length() + 1);
 
 		// Figure out StaticJsonBuffer
 		StaticJsonBuffer<1024> jsonBuffer;
@@ -77,41 +31,30 @@ void loop() {
 
 		// Test if parsing succeeds.
 		if (!root.success()) {
-			Serial.println("parseObject() failed");
+			Serial.println("{3:parseObject() failed}");
 			free(json_str);
 			return;
 		}
 
-		const char *instr_type = root["type"];
-		Serial.println(instr_type);
+		String instr_type = root["type"];
+		Serial.println("{2:"+instr_type+"}");
+		
+		// if (strcmp("setup", instr_type) == 0) {
+		if (instr_type == "setup") {
+			Serial.println("{2:Configuring motor setups...}");
+			configure_motors(root["data"]);
+			Serial.println("{1:Setup complete!}");
 
-		if (strcmp("setup", instr_type) == 0) {
-			Serial.println("Configuring motor setups...");
-			JsonObject& data = root["data"];
-			configure_motors(data);
-		} else if (strcmp("move", instr_type) == 0) {
-			Serial.println("Moving motors ...");
-			JsonObject& data = root["data"]["steps"];
-			handle_move(data);
-		} else if (strcmp("moves", instr_type) == 0) {
-			Serial.println("Moving multiple motors <instr not implemented> ...");
+		} else if (instr_type == "move") {
+			Serial.println("{2:Executing move...}");
+			handle_move(root["data"]["steps"]);
+			Serial.println("{1:Move complete!}");
+
+		} else if (instr_type == "moves") {
+			Serial.println("{2:Executing multi-moves...}");
+			handle_multiple_moves(root["data"]["stepsArray"]);
+			Serial.println("{1:Multi-moves complete!}");
 		}
-
-		// const char *inst = root["inst"];
-		// Serial.println(inst);
-	
-		// if (strcmp("move", inst) == 0) {
-		// 	Serial.println("moving one stepper...");
-		// 	JsonObject& steps = root["steps"];
-		// 	handle_move(steps);
-		// }
-
-		// if (strcmp("moves", inst) == 0) {
-		// 	Serial.println("moving multi steppers...");
-		// 	JsonArray& steps_arr = root["steps"];
-		// 	handle_multiple_moves(steps_arr);
-		// }
-
 		free(json_str);
 	}
 }
@@ -119,38 +62,39 @@ void loop() {
 
 // Rewrite handle_move so that it works
 void configure_motors(JsonObject& data) {
-	NUM_STAGES = data["stages"];
+	NUM_STAGES = atoi(data["stages"]);
 	// Serial.println(NUM_STAGES); // value correct
 	JsonArray& motors = data["motors"].as<JsonArray>();
 	int index = 0;
 
 	for (JsonObject& motor : motors) {
 		String name = motor["name"];
-		int step = atoi(motor["step"]);
-		int dir = atoi(motor["dir"]);
 		stage_names[index] = name;
+		Serial.println(index);
+		StepperPins mapping = pin_map(atoi(motor["map"]));
+		
 		// Serial.println(index); // values correct
 		// Serial.println(name + ": " + step + ", " + dir); // values correct
-		phys_motors[index] = AccelStepper(AccelStepper::DRIVER, step, dir);
+		phys_motors[index] = AccelStepper(AccelStepper::DRIVER, mapping.step, mapping.dir);
 		set_motor_kinematics(index++);
 	}
 
-	Serial.println("Configuration Complete!");
+	// Serial.println("{2:Testing Movement...}");
+	// test_motors();
+	// Serial.println("{2:Testing Complete!}");
+}
 
-	Serial.println("Testing Movement...");
-	test_motors();
-	// long coords0[3] = { 10 * 328, 10 * 328, 4 * 328 };
-	// MultiStepper steppers;
-	// for (int i = 0; i < NUM_STAGES; i++) steppers.addStepper(phys_motors[i]);
 
-	// steppers.moveTo(coords0);
-	// steppers.runSpeedToPosition();
-	// delay(100);
-
-	// for (int i = 0; i < NUM_STAGES; i++) {
-	// 	phys_motors[i] = AccelStepper(AccelStepper::DRIVER, motors[i]["step"], motors[i]["dir"]);
-	// 	set_motor_kinematics(index++);
-	// }
+StepperPins pin_map(int num) {
+	StepperPins output;
+	switch(num) {
+		case 0: output.step = STEP0; output.dir = DIR0; Serial.println("{2:Map to Step0 & Dir0 w/ " + String(num)); break;
+		case 1: output.step = STEP1; output.dir = DIR1; Serial.println("{2:Map to Step1 & Dir1 w/ " + String(num)); break;
+		case 2: output.step = STEP2; output.dir = DIR2; Serial.println("{2:Map to Step2 & Dir2 w/ " + String(num)); break;
+		case 3: output.step = STEP3; output.dir = DIR3; Serial.println("{2:Map to Step3 & Dir3 w/ " + String(num)); break;
+		default: output.step = STEP0; output.dir = DIR0; Serial.println("{2:Map to Step3 & Dir3 w/ " + String(num));break;
+	}
+	return output;
 }
 
 
@@ -164,12 +108,20 @@ void set_motor_kinematics(int index) {
 void handle_move(JsonObject& steps) {
 	MultiStepper steppers;
 	long motor_steps[MAX_STAGES]; // not allowed to set variable NUM_STAGES
-	int index = 0;
-
+	// int step_i = 0;
+	// test_motors();
 	for (auto motor : steps) {
-		Serial.println(motor.key);
-		steppers.addStepper(get_phys_motor(motor.key));
-		motor_steps[index++] = MS_FACTOR * motor.value.as<long>();
+		// Serial.println(motor.key);
+		int index = get_motor_index(motor.key);
+		if (index != -1) {
+			// Serial.println("{2:There's a match!}");
+			// Serial.println("{2:index: " + String(index) + "}");
+			steppers.addStepper(phys_motors[index]);
+			motor_steps[index++] = MS_FACTOR * motor.value.as<long>();
+			// Serial.println("{2:" + String(motor_steps[index-1]) + "}");
+		} else {
+			Serial.println("{2:There isn't a match!}");
+		}
 	}
 
 	steppers.moveTo(motor_steps);
@@ -178,26 +130,25 @@ void handle_move(JsonObject& steps) {
 }
 
 
-void handle_multiple_moves(JsonArray& steps) {
+void handle_multiple_moves(JsonArray& moves) {
 	// Note that steps is a _list_ of steps, unlike handle_move
-	for (JsonObject& step : steps) {
-		handle_move(step);
+	for (JsonObject& move : moves) {
+		handle_move(move);
 	}
 }
 
 
-AccelStepper& get_phys_motor(String stage_name) {
+int get_motor_index(String stage_name) {
 	int index = string_index_of(stage_name);
 	if (index == -1) {
-		Serial.println("Error: cannot find stage");
-		return phys_motors[0];
+		Serial.println("{3:Error: cannot find stage}");
 	}
-	return phys_motors[index];
+	return index;
 }
 
 
 int string_index_of(String name) {
-	for (int i = 0; i < NUM_STAGES; i++) {
+	for (uint8_t i = 0; i < NUM_STAGES; i++) {
 		if (name == stage_names[i]) return i;
 	}
 	return -1;
@@ -205,7 +156,7 @@ int string_index_of(String name) {
 
 
 void zero_motors(void) {
-	for (int i = 0; i < NUM_STAGES; i += 1) {
+	for (uint8_t i = 0; i < NUM_STAGES; i++) {
 		phys_motors[i].setCurrentPosition(0);
 	}
 }
@@ -232,4 +183,3 @@ void test_motors(void) {
 	zero_motors();
 	delay(100);
 }
-// { "inst" : "move", "steps" : { "x1" : "50" } }
